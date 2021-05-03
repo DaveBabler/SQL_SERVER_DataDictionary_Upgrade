05821d46dda1671e05db1fa072bd6a011d0d43b8
Dave Babler <dbabler@mezrahconsulting.com>
2021-05-03 17:52:44 -0400

    FIX LOGIC error in schema object assignment

--- Utility/SCHEMAS/DD/PROCEDURES/DD.DBSchemaObjectAssignment.sql
+++ Utility/SCHEMAS/DD/PROCEDURES/DD.DBSchemaObjectAssignment.sql
@@ -1,8 +1,10 @@
 USE [Utility]
 GO
+
 /****** Object:  StoredProcedure [DD].[DBSchemaObjectAssignment]    Script Date: 4/28/2021 3:32:59 PM ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
 
@@ -18,7 +20,10 @@ GO
 -- 					2 sp_executesql -- system procedure, dynamic SQL.
 --
 -- =============================================
-ALTER	  PROCEDURE [DD].[DBSchemaObjectAssignment]
+CREATE
+	OR
+
+ALTER PROCEDURE [DD].[DBSchemaObjectAssignment]
 	-- Add the parameters for the stored procedure here
 	@strQualifiedObjectBeingCalled NVARCHAR(200) --64*3+UP TO 2 PERIODS TO NEXT OCTET
 	, @ustrDatabaseName NVARCHAR(64) = NULL OUTPUT
@@ -33,13 +38,13 @@ BEGIN TRY
 	DECLARE @bitDatabaseExists BIT = 0;
 	DECLARE @bitSchemaExists BIT = 0;
 	DECLARE @intNumPiecesEntered INT = 0;
-	DECLARE @ustrDefaultSchema NVARCHAR(64) = 'XXPPQQZZ';--WE WOULD FILL THIS IN.
-	DECLARE @ustrDefaultDatabase NVARCHAR(64) = 'XXPPQQZZ';
+	DECLARE @ustrDefaultSchema NVARCHAR(64) = 'dbo';--WE WOULD FILL THIS IN.
+	DECLARE @ustrDefaultDatabase NVARCHAR(64) = 'mapbenefits';
 	DECLARE @intDesiredPiece INT;
 	DECLARE @ustrSQLToExecute NVARCHAR(4000);-- highly unlikely it would be longer in this proc.
 	DECLARE @uDynamSQLParams NVARCHAR(2000) = N'@intDesiredPiece_ph INT, @ustrObjectFromTemp_ph NVARCHAR(64) OUTPUT';
-		--we use this "p"lace "h"older in dyanmicSQL.
 
+	--we use this "p"lace "h"older in dyanmicSQL.
 	CREATE TABLE #tblObjectBreakdown (
 		intPosition INT
 		, ustrObjectPiece NVARCHAR(64)
@@ -49,6 +54,7 @@ BEGIN TRY
 	--GET MAX COUNT OF ROWID USE THAT TO DETERMINE LOGIC  IF NO PERIODS SKIP LOGIC USE DEFAULTS
 	SET @intDelimCountChecker = CHARINDEX('.', @strQualifiedObjectBeingCalled);
 
+	PRINT @intDelimCountChecker;
 
 	IF @intDelimCountChecker > 0
 	BEGIN
@@ -59,11 +65,13 @@ BEGIN TRY
 
 		SELECT @intNumPiecesEntered = MAX(intPosition)
 		FROM #tblObjectBreakdown;
-				SET @ustrSQLTOExecute = 
-				N'SELECT @ustrObjectFromTemp_ph = ustrObjectPiece 
+
+		SET @ustrSQLTOExecute = 
+			N'SELECT @ustrObjectFromTemp_ph = ustrObjectPiece 
                                             FROM #tblObjectBreakdown
                                             WHERE intPosition = @intDesiredPiece_ph'
-				;
+			;
+
 		IF @intNumPiecesEntered = 3
 			/*Ostensibly all 3 pieces are entered so let's check that and then assign them */
 		BEGIN
@@ -71,19 +79,20 @@ BEGIN TRY
                     * In this case, we are ok because all we are using a temporary table that WE built, and we are assigning variables
                     * and as such it should be cool.  Dave Babler 2020-11-15 */
 			--rip through the pieces and assign them to outputs
-	
 			SET @intDesiredPiece = 1;
 
 			EXEC sp_executesql @ustrSQLTOExecute
 				, @uDynamSQLParams
 				, @ustrObjectFromTemp_ph = @ustrDataBaseName OUTPUT --grabs database name
 				, @intDesiredPiece_ph = @intDesiredPiece;
+
 			SET @intDesiredPiece = 2;
 
 			EXEC sp_executesql @ustrSQLTOExecute
 				, @uDynamSQLParams
 				, @ustrObjectFromTemp_ph = @ustrSchemaName OUTPUT --grabs schema name
 				, @intDesiredPiece_ph = @intDesiredPiece;
+
 			SET @intDesiredPiece = 3;
 
 			EXEC sp_executesql @ustrSQLTOExecute
@@ -109,22 +118,19 @@ BEGIN TRY
 				, @ustrObjectFromTemp_ph = @ustObjectOrTableName OUTPUT --grabs object name
 				, @intDesiredPiece_ph = @intDesiredPiece;
 		END
-			ELSE
+	END
+	ELSE
 	BEGIN
 		SET @ustrSchemaName = @ustrDefaultSchema;
 		SET @ustrDatabaseName = @ustrDefaultDatabase;
 		SET @ustObjectOrTableName = @strQualifiedObjectBeingCalled;
-
 			--check info schema to make sure the schmea and the db actually exist.
 	END
 
-	END
 	SET NOCOUNT OFF;
 END TRY
 
-
 BEGIN CATCH
-
 	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK (
 		[DatabaseName]
 		, [UserName]
@@ -148,9 +154,7 @@ BEGIN CATCH
 		, GETDATE()
 		);
 END CATCH;
-
-
---^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^TESTING BLOCK^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^TESTING BLOCK^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 	/*   
 		DECLARE @ustrDatabaseName NVARCHAR(64)
 			, @ustrSchemaName NVARCHAR(64)
@@ -166,7 +170,7 @@ END CATCH;
 			, @ustObjectOrTableName;
 
  */
-
---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-
+	--vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 GO
+
+
