d1274a4d856bf98e32b028477871685ff8e4ba52
Dave Babler <dbabler@anazaohealth.com>
2021-05-19 16:29:42 -0400

    add try catch example

---
+++ Documentation/Example TryCatch With CustomLog.sql
@@ -0,0 +1,29 @@
+BEGIN TRY
+DECLARE @intShoes INT
+SELECT @intShoes = 1/0;
+END TRY
+
+BEGIN CATCH
+	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK (
+		[DatabaseName]
+		, [UserName]
+		, [ErrorNumber]
+		, [ErrorState]
+		, [ErrorSeverity]
+		, [ErrorLine]
+		, [ErrorProcedure]
+		, [ErrorMessage]
+		, [ErrorDateTime]
+		)
+	VALUES (
+		DB_NAME()
+		, SUSER_SNAME()
+		, ERROR_NUMBER()
+		, ERROR_STATE()
+		, ERROR_SEVERITY()
+		, ERROR_LINE()
+		, ERROR_PROCEDURE() + OBJECT_NAME(@@PROCID)
+		, ERROR_MESSAGE()
+		, GETDATE()
+		);
+END CATCH;
