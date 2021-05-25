37d37f05f3cebdd94d5e41a6350fe1e7046c53dc
Dave Babler <dbabler@anazaohealth.com>
2021-05-25 09:14:21 -0400

    add transactional try catch example

---
+++ Documentation/ExampleTryCatchTransaction.sql
@@ -0,0 +1,59 @@
+
+BEGIN TRY
+	SET XACT_ABORT ON;-- 
+	BEGIN TRANSACTION 
+
+    UPDATE NECRONOMICON
+    SET DemonsSummonedPerActivation = (12/0);
+
+    COMMIT;
+
+	SET XACT_ABORT OFF;-- 
+END TRY
+
+BEGIN CATCH
+	IF (XACT_STATE()) = - 1 --test to see if we cannot commit
+	BEGIN
+		PRINT N'The transaction is in an uncommittable state.' + 'Rolling back transaction.'
+
+		ROLLBACK TRANSACTION;
+
+		SET XACT_ABORT OFF;
+			-- currently our database has this as default so we need to double check it turns off on fail or success
+	END;
+
+	IF (XACT_STATE()) = 1 -- Test if the transaction is committable.  
+	BEGIN
+		PRINT N'The transaction is committable.' + 'Committing transaction.'
+
+		COMMIT TRANSACTION;
+
+		SET XACT_ABORT OFF;
+			-- currently our database has this as default so we need to triple check it turns off on fail or success
+	END;
+
+	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK (
+		[DatabaseName]
+		, [UserName]
+		, [ErrorNumber]
+		, [ErrorState]
+		, [ErrorSeverity]
+		, [ErrorLine]
+		, [ErrorProcedure]
+		, [ErrorMessage]
+		, [ErrorDateTime]
+		)
+	VALUES (
+		DB_NAME()
+		, SUSER_SNAME()
+		, ERROR_NUMBER()
+		, ERROR_STATE()
+		, ERROR_SEVERITY()
+		, ERROR_LINE()
+		, ERROR_PROCEDURE()
+		, ERROR_MESSAGE()
+		, GETDATE()
+		);
+THROW;
+END CATCH;
+GO
