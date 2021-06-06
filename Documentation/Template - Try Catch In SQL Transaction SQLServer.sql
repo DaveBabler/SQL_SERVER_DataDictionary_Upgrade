1469a74bc54695e3a6f95812a702e1559060bb28
Dave Babler <dbabler@anazaohealth.com>
2021-06-06 12:03:54 -0400

    EVEN BETTER TRY CATCH TRANSACTION TEST

--- Documentation/Template - Try Catch In SQL Transaction SQLServer.sql
+++ Documentation/Template - Try Catch In SQL Transaction SQLServer.sql
@@ -1,25 +1,35 @@
 BEGIN TRY
-	SET XACT_ABORT ON;-- 
+	SET XACT_ABORT ON; -- 
 
 	BEGIN TRANSACTION
 
-	UPDATE NECRONOMICON
-	SET DemonsSummonedPerActivation = (12 / 0);
+	CREATE TABLE #NECRONOMICON
+	(
+		DemonSummonActivation INT IDENTITY(1, 1) PRIMARY KEY
+	  , DemonsSummonedPerActivation INT
+	)
+
+	INSERT INTO #NECRONOMICON (DemonsSummonedPerActivation)
+	VALUES
+		(12)
+	  , (5)
+	  , (99999999);
+
+	UPDATE #NECRONOMICON SET DemonsSummonedPerActivation = (12 / 0);
 
 	COMMIT;
 
-	SET XACT_ABORT OFF;-- 
+	SET XACT_ABORT OFF; -- 
 END TRY
-
 BEGIN CATCH
-	IF (XACT_STATE()) = - 1 --test to see if we cannot commit
+	IF (XACT_STATE()) = -1 --test to see if we cannot commit
 	BEGIN
 		PRINT N'The transaction is in an uncommittable state.' + 'Rolling back transaction.'
 
 		ROLLBACK TRANSACTION;
 
 		SET XACT_ABORT OFF;
-			-- currently our database has this as default so we need to double check it turns off on fail or success
+	-- currently our database has this as default so we need to double check it turns off on fail or success
 	END;
 
 	IF (XACT_STATE()) = 1 -- Test if the transaction is committable.  
@@ -29,44 +39,37 @@ BEGIN CATCH
 		COMMIT TRANSACTION;
 
 		SET XACT_ABORT OFF;
-			-- currently our database has this as default so we need to triple check it turns off on fail or success
+	-- currently our database has this as default so we need to triple check it turns off on fail or success
 	END;
 
-	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK (
-		[DatabaseName]
-		, [UserName]
-		, [ErrorNumber]
-		, [ErrorState]
-		, [ErrorSeverity]
-		, [ErrorLine]
-		, [ErrorProcedure]
-		, [ErrorMessage]
-		, [ErrorDateTime]
+	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK
+		(
+			[DatabaseName]
+		  , [UserName]
+		  , [ErrorNumber]
+		  , [ErrorState]
+		  , [ErrorSeverity]
+		  , [ErrorLine]
+		  , [ErrorProcedure]
+		  , [ErrorMessage]
+		  , [ErrorDateTime]
 		)
-	VALUES (
-		DB_NAME()
-		, SUSER_SNAME()
-		, ERROR_NUMBER()
-		, ERROR_STATE()
-		, ERROR_SEVERITY()
-		, ERROR_LINE()
-		, ISNULL(ERROR_PROCEDURE(), CONCAT (
-				ERROR_PROCEDURE()
-				, ' '
-				, CONCAT (
-					DB_NAME()
-					, '.'
-					, SCHEMA_NAME()
-					, '.'
-					, OBJECT_NAME(@@PROCID)
-					)
-				))
-		, ERROR_MESSAGE()
-		, GETDATE()
+	VALUES
+		(
+			DB_NAME()
+		  , SUSER_SNAME()
+		  , ERROR_NUMBER()
+		  , ERROR_STATE()
+		  , ERROR_SEVERITY()
+		  , ERROR_LINE()
+		  , ISNULL(
+					  ERROR_PROCEDURE()
+					, CONCAT(ERROR_PROCEDURE(), ' ', CONCAT(DB_NAME(), '.', SCHEMA_NAME(), '.', OBJECT_NAME(@@PROCID)))
+				  )
+		  , ERROR_MESSAGE()
+		  , GETDATE()
 		);
 
 	THROW;
 END CATCH;
 GO
-
-
