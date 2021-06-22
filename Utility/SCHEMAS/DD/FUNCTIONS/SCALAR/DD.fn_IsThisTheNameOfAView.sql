d03944843dbf44c8d7f2fa84b24f65846aa26f15
Dave Babler <dbabler@anazaohealth.com>
2021-06-21 20:04:30 -0400

    upates to dd procs

--- Utility/SCHEMAS/DD/FUNCTIONS/SCALAR/DD.fn_IsThisTheNameOfAView.sql
+++ Utility/SCHEMAS/DD/FUNCTIONS/SCALAR/DD.fn_IsThisTheNameOfAView.sql
@@ -1,5 +1,9 @@
-USE [Utility];
+SET ANSI_NULLS ON
 GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 -- ==========================================================================================
 -- Author:		Dave Babler
 -- Create date: 2021-01-31
@@ -8,13 +12,12 @@ GO
 -- 				Since this not going to be a program that will be called typically outside of another proc 
 -- 				have left it's name in the traditional fn_ convention.
 -- ==========================================================================================
-CREATE
-	OR
 
-ALTER FUNCTION [DD].[fn_IsThisTheNameOfAView] (
+CREATE FUNCTION [DD].[fn_IsThisTheNameOfAView]
+(
 	-- Add the parameters for the function here
 	@ustrName NVARCHAR(64)
-	)
+)
 RETURNS BIT
 AS
 BEGIN
@@ -24,17 +27,13 @@ BEGIN
 	-- First Check to see if the 'table' name starts with a V if not assume not view and throw false flag
 	IF LOWER(LEFT(@ustrName, 1)) = 'v'
 	BEGIN
-		SET @bitFlag = CASE 
-				WHEN LOWER(LEFT(@ustrName, 4)) = 'view'
-					THEN 1
-				WHEN LOWER(LEFT(@ustrName, 3)) = 'viw'
-					THEN 1
-				WHEN LOWER(LEFT(@ustrName, 2)) = 'vw'
-					THEN 1
-				WHEN LOWER(LEFT(@ustrName, 2)) = 'v_'
-					THEN 1
-				ELSE 0
-				END
+		SET @bitFlag = CASE
+						   WHEN LOWER(LEFT(@ustrName, 4)) = 'view' THEN 1
+						   WHEN LOWER(LEFT(@ustrName, 3)) = 'viw' THEN 1
+						   WHEN LOWER(LEFT(@ustrName, 2)) = 'vw' THEN 1
+						   WHEN LOWER(LEFT(@ustrName, 2)) = 'v_' THEN 1
+						   ELSE 0
+					   END
 	END
 	ELSE
 	BEGIN
@@ -44,13 +43,4 @@ BEGIN
 	-- Return the result of the function
 	RETURN @bitFlag
 END
-GO
-
-
---^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^TESTING BLOCK^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-/*
-	 SELECT UTL.fn_IsThisTheNameOfAView('V_ofDATA'); 
-*/
---vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-
 GO
--- Utility/SCHEMAS/DD/PROCEDURES/DD.TableShowComment.sql
+++ Utility/SCHEMAS/DD/PROCEDURES/DD.TableShowComment.sql
@@ -1,10 +1,3 @@
-USE Utility
-GO
--- Delete old procedure update to noun verb form
-DROP PROCEDURE IF EXISTS DD.ShowTableComment;
-GO
-
--- ================================================
 SET ANSI_NULLS ON
 GO
 
@@ -17,183 +10,177 @@ GO
 -- Last Edited By:	Dave Babler
 -- Last Updated:	2021-04-24
 -- Description:		Checks to see if table comments exist
--- Subprocedures: 	1. [Utility].[UTL].[fn_SuppressOutput]
--- 					2. [Utility].[DD].[DBSchemaObjectAssignment]
--- 					3. [Utility].[DD].[TableExist]
---  				4. [Utility].[DD].[fn_IsThisTheNameOfAView]
+-- Subprocedures: 	1. [$(DatabaseName)].[UTL].[fn_SuppressOutput]
+-- 					2. [$(DatabaseName)].[DD].[DBSchemaObjectAssignment]
+-- 					3. [$(DatabaseName)].[DD].[TableExist]
+--  				4. [$(DatabaseName)].[DD].[fn_IsThisTheNameOfAView]
 -- ==========================================================================================
-CREATE
-	OR
 
-ALTER PROCEDURE DD.TableShowComment @ustrFQON NVARCHAR(200)
-	, @boolOptionalSuccessFlag BIT = NULL OUTPUT
-	, @strOptionalMessageOut NVARCHAR(320) = NULL OUTPUT
-	/** The success flag will be used when passing this to other procedures to see if table comments exist.
+CREATE OR ALTER PROCEDURE [DD].[TableShowComment]
+	@ustrFQON NVARCHAR(200)
+  , @boolOptionalSuccessFlag BIT = NULL OUTPUT
+  , @strOptionalMessageOut NVARCHAR(320) = NULL OUTPUT
+/** The success flag will be used when passing this to other procedures to see if table comments exist.
 	 * The optional message out will be used when passing from proc to proc to make things more proceduralized.
 	 * --Dave Babler 08/26/2020  */
 AS
-DECLARE @ustrMessageOut NVARCHAR(320)
-	, @intRowCount INT
-	, @bitSuppressVisualOutput BIT
-	, @bitIsThisAView BIT
-	, @bitExistFlag BIT
-	, @ustrDatabaseName NVARCHAR(64)
-	, @ustrSchemaName NVARCHAR(64)
-	, @ustrTableOrObjName NVARCHAR(64)
-	, @ustrViewOrTable NVARCHAR(8)
-	, @dSQLCheckForComment NVARCHAR(MAX)
-	, @dSQLPullComment NVARCHAR(MAX)
-	, @dSQLPullCommentParameters NVARCHAR(MAX)
-	, @dSQLInternalVariantOutput SQL_VARIANT;
+	DECLARE @ustrMessageOut NVARCHAR(320)
+		  , @intRowCount INT
+		  , @bitSuppressVisualOutput BIT
+		  , @bitIsThisAView BIT
+		  , @bitExistFlag BIT
+		  , @ustrDatabaseName NVARCHAR(64)
+		  , @ustrSchemaName NVARCHAR(64)
+		  , @ustrTableOrObjName NVARCHAR(64)
+		  , @ustrViewOrTable NVARCHAR(8)
+		  , @dSQLCheckForComment NVARCHAR(MAX)
+		  , @dSQLPullComment NVARCHAR(MAX)
+		  , @dSQLPullCommentParameters NVARCHAR(MAX)
+		  , @dSQLInternalVariantOutput SQL_VARIANT;
 
-CREATE TABLE #__SuppressOutputTableShowComment (SuppressedOutput VARCHAR(MAX))
+	CREATE TABLE #__SuppressOutputTableShowComment (SuppressedOutput VARCHAR(MAX))
 
-BEGIN TRY
-	/**First with procedures that are stand alone/embedded hybrids, determine if we need to suppress output by 
+	BEGIN TRY
+		/**First with procedures that are stand alone/embedded hybrids, determine if we need to suppress output by 
   * populating the data for that variable 
   * --Dave Babler */
-	SELECT @bitSuppressVisualOutput = [Utility].[UTL].[fn_SuppressOutput]();
+		SELECT @bitSuppressVisualOutput = [Utility].[UTL].[fn_SuppressOutput]();
 
-	--first blow apart the fully qualified object name
-	EXEC [Utility].[DD].[DBSchemaObjectAssignment] @ustrFQON
-		, @ustrDatabaseName OUTPUT
-		, @ustrSchemaName OUTPUT
-		, @ustrTableOrObjName OUTPUT;
+		--first blow apart the fully qualified object name
+		EXEC [Utility].[DD].[DBSchemaObjectAssignment] @ustrFQON
+													 , @ustrDatabaseName OUTPUT
+													 , @ustrSchemaName OUTPUT
+													 , @ustrTableOrObjName OUTPUT;
 
-	/** Next Check to see if the name is for a view instead of a table, alter the function to fit your agency's naming conventions
+		/** Next Check to see if the name is for a view instead of a table, alter the function to fit your agency's naming conventions
 		 * Not necessary to check this beforehand as the previous calls will work for views and tables due to how
 		 * INFORMATION_SCHEMA is set up.  Unfortunately from this point on we'll be playing with Microsoft's sys tables
 		  */
-	SET @bitIsThisAView = [Utility].[DD].[fn_IsThisTheNameOfAView](@ustrTableOrObjName);
+		SET @bitIsThisAView = [Utility].[DD].[fn_IsThisTheNameOfAView](@ustrTableOrObjName);
 
-	IF @bitIsThisAView = 0
-		SET @ustrViewOrTable = 'TABLE';
-	ELSE
-		SET @ustrViewOrTable = 'VIEW';
+		IF @bitIsThisAView = 0
+			SET @ustrViewOrTable = 'BASE TABLE';
+		ELSE
+			SET @ustrViewOrTable = 'VIEW';
 
-	EXEC [Utility].[DD].[TableExist] @ustrTableOrObjName
-		, @ustrDatabaseName
-		, @ustrSchemaName
-		, @bitExistFlag OUTPUT
-		, @ustrMessageOut OUTPUT;
+		EXEC [Utility].[DD].[TableExist] @ustrTableOrObjName
+									   , @ustrDatabaseName
+									   , @ustrSchemaName
+									   , @bitExistFlag OUTPUT
+									   , @ustrMessageOut OUTPUT;
 
-	PRINT @ustrMessageOut;
+		PRINT @ustrMessageOut;
 
-	IF @bitExistFlag = 1
-	BEGIN
-		/**Check to see if the table has the extened properties on it.
+		IF @bitExistFlag = 1
+		BEGIN
+			/**Check to see if the table has the extened properties on it.
                         *If it does not  will ultimately ask someone to please create 
                         * the comment on the table -- Babler */
-		SET @dSQLCheckForComment = N' SELECT 1
-									FROM ' + QUOTENAME(@ustrDataBaseName) + '.sys.extended_properties' + 
-			' WHERE [major_id] = OBJECT_ID(' + '''' + @ustrDatabaseName + '.' + @ustrSchemaName + '.' + @ustrTableOrObjName + '''' + ')' 
-			+ ' AND [name] = N''MS_Description''
+			SET @dSQLCheckForComment = N' SELECT 1
+									FROM ' + QUOTENAME(@ustrDataBaseName) + '.sys.extended_properties'
+									   + ' WHERE [major_id] = OBJECT_ID(' + '''' + @ustrDatabaseName + '.'
+									   + @ustrSchemaName + '.' + @ustrTableOrObjName + '''' + ')'
+									   + ' AND [name] = N''MS_Description''
 										AND [minor_id] = 0';
 
-		INSERT INTO #__SuppressOutputTableShowComment
-		EXEC sp_executesql @dSQLCheckForComment;
+			INSERT INTO #__SuppressOutputTableShowComment
+			EXEC sp_executesql @dSQLCheckForComment;
 
-		SET @intRowCount = @@ROWCOUNT;
+			SET @intRowCount = @@ROWCOUNT;
 
-		IF @intRowCount != 0
-		BEGIN
-			SET @dSQLPullComment = N'
+			IF @intRowCount != 0
+			BEGIN
+				SET @dSQLPullComment = N'
 								
 								SELECT   @ustrMessageOutTemp  = epExtendedProperty
-								FROM ' 
-				+ QUOTENAME(@ustrDataBaseName) + 
-				'.INFORMATION_SCHEMA.TABLES AS t
+								FROM ' + QUOTENAME(@ustrDataBaseName)
+									   + '.INFORMATION_SCHEMA.TABLES AS t
 								INNER JOIN (
 									
-									SELECT OBJECT_NAME(ep.major_id, DB_ID(' 
-				+ '''' + @ustrDataBaseName + '''' + 
-				')) AS [epTableName]
+									SELECT OBJECT_NAME(ep.major_id, DB_ID(' + '''' + @ustrDataBaseName + ''''
+									   + ')) AS [epTableName]
 										, CAST(ep.Value AS NVARCHAR(320)) AS [epExtendedProperty]
-									FROM ' 
-				+ QUOTENAME(@ustrDataBaseName) + 
-				'.sys.extended_properties ep
+									FROM ' + QUOTENAME(@ustrDataBaseName)
+									   + '.sys.extended_properties ep
 									WHERE ep.name = N''MS_Description'' 
 										AND ep.minor_id = 0 
 									
 								) AS tp
 									ON t.TABLE_NAME = tp.epTableName
-								WHERE TABLE_TYPE = ''BASE TABLE''
-								AND tp.epTableName = @ustrTableOrObjName
+								WHERE TABLE_TYPE = ' + '''' + @ustrViewOrTable + ''''
+									   + 'AND tp.epTableName = @ustrTableOrObjName
 									AND t.TABLE_CATALOG = @ustrDatabaseName
-									AND t.TABLE_SCHEMA = @ustrSchemaName'
-				;
+									AND t.TABLE_SCHEMA = @ustrSchemaName';
 
-			PRINT @dSQLPullComment
+				PRINT @dSQLPullComment
 
-			SET @dSQLPullCommentParameters = 
-				N' @ustrDatabaseName NVARCHAR(64)
+				SET @dSQLPullCommentParameters = N' @ustrDatabaseName NVARCHAR(64)
+				, @ustrSchemaName NVARCHAR(64)
+				, @ustrTableOrObjName NVARCHAR(64)
+				, @ustrMessageOutTemp NVARCHAR(320) OUTPUT';
+
+				EXECUTE sp_executesql @dSQLPullComment
+									, N' @ustrDatabaseName NVARCHAR(64)
 				, @ustrSchemaName NVARCHAR(64)
 				, @ustrTableOrObjName NVARCHAR(64)
 				, @ustrMessageOutTemp NVARCHAR(320) OUTPUT'
-				;
+									, @ustrDatabaseName = @ustrDatabaseName
+									, @ustrSchemaName = @ustrSchemaName
+									, @ustrTableOrObjName = @ustrTableOrObjName
+									, @ustrMessageOutTemp = @ustrMessageOut OUTPUT;
 
-			EXECUTE sp_executesql @dSQLPullComment
-				, 
-				N' @ustrDatabaseName NVARCHAR(64)
-				, @ustrSchemaName NVARCHAR(64)
-				, @ustrTableOrObjName NVARCHAR(64)
-				, @ustrMessageOutTemp NVARCHAR(320) OUTPUT'
-				, @ustrDatabaseName = @ustrDatabaseName
-				, @ustrSchemaName = @ustrSchemaName
-				, @ustrTableOrObjName = @ustrTableOrObjName
-				, @ustrMessageOutTemp = @ustrMessageOut OUTPUT;
+				PRINT @ustrMessageOut
 
-			PRINT @ustrMessageOut
+				SET @boolOptionalSuccessFlag = 1; --Let any calling procedures know that there is in fact
+				SET @strOptionalMessageOut = @ustrMessageOut;
+			END
+			ELSE
+			BEGIN
+				SET @boolOptionalSuccessFlag = 0; --let any proc calling know that there is no table comments yet.
+				SET @ustrMessageOut = @ustrDataBaseName + '.' + @ustrSchemaName + '.' + @ustrTableOrObjName
+									  + N' currently has no comments please use [Utility].DD.TableAddComment to add comments!';
+				SET @strOptionalMessageOut = @ustrMessageOut;
+			END
 
-			SET @boolOptionalSuccessFlag = 1;--Let any calling procedures know that there is in fact
-			SET @strOptionalMessageOut = @ustrMessageOut;
+			IF @bitSuppressVisualOutput = 0
+			BEGIN
+				SELECT @ustrTableOrObjName AS 'Table Name'
+					 , @ustrMessageOut AS 'TableComment';
+			END
 		END
 		ELSE
 		BEGIN
-			SET @boolOptionalSuccessFlag = 0;--let any proc calling know that there is no table comments yet.
-			SET @ustrMessageOut = @ustrDataBaseName + '.' + @ustrSchemaName + '.' + @ustrTableOrObjName + 
-				N' currently has no comments please use Utility.DD.TableAddComment to add comments!';
-			SET @strOptionalMessageOut = @ustrMessageOut;
+			SET @ustrMessageOut = ' The table you typed in: ' + @ustrTableOrObjName + ' '
+								  + 'is invalid, check spelling, try again? ';
+
+			SELECT @ustrMessageOut AS 'NON_LOGGED_ERROR_MESSAGE'
 		END
-
-		IF @bitSuppressVisualOutput = 0
-		BEGIN
-			SELECT @ustrTableOrObjName AS 'Table Name'
-				, @ustrMessageOut AS 'TableComment';
-		END
-	END
-	ELSE
-	BEGIN
-		SET @ustrMessageOut = ' The table you typed in: ' + @ustrTableOrObjName + ' ' + 'is invalid, check spelling, try again? '
-			;
-
-		SELECT @ustrMessageOut AS 'NON_LOGGED_ERROR_MESSAGE'
-	END
-END TRY
-
-BEGIN CATCH
-	INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK (
-		[DatabaseName]
-		, [UserName]
-		, [ErrorNumber]
-		, [ErrorState]
-		, [ErrorSeverity]
-		, [ErrorLine]
-		, [ErrorProcedure]
-		, [ErrorMessage]
-		, [ErrorDateTime]
-		)
-	VALUES (
-		DB_NAME()
-		, SUSER_SNAME()
-		, ERROR_NUMBER()
-		, ERROR_STATE()
-		, ERROR_SEVERITY()
-		, ERROR_LINE()
-		, ERROR_PROCEDURE()
-		, ERROR_MESSAGE()
-		, GETDATE()
-		);
+	END TRY
+	BEGIN CATCH
+		INSERT INTO CustomLog.ERR.DB_EXCEPTION_TANK
+			(
+				[DatabaseName]
+			  , [UserName]
+			  , [ErrorNumber]
+			  , [ErrorState]
+			  , [ErrorSeverity]
+			  , [ErrorLine]
+			  , [ErrorProcedure]
+			  , [ErrorMessage]
+			  , [ErrorDateTime]
+			)
+		VALUES
+			(
+				DB_NAME()
+			  , SUSER_SNAME()
+			  , ERROR_NUMBER()
+			  , ERROR_STATE()
+			  , ERROR_SEVERITY()
+			  , ERROR_LINE()
+			  , ERROR_PROCEDURE()
+			  , ERROR_MESSAGE()
+			  , GETDATE()
+			);
 
 	--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--TESTING BLOCK--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 	/* 
@@ -201,11 +188,11 @@ BEGIN CATCH
 	DECLARE @boolOptionalSuccessFlag BIT = NULL;
 	DECLARE @strOptionalMessageOut NVARCHAR(320) = NULL;
 
-	EXEC Utility.DD.TableShowComment @ustrFullyQualifiedTable
+	EXEC [$(DatabaseName)].DD.TableShowComment @ustrFullyQualifiedTable
 		, @boolOptionalSuccessFlag OUTPUT
 		, @strOptionalMessageOut OUTPUT;
 
-	SELECT @boolOptionalSuccessFlag AS N'Success 🚩'
+	SELECT @boolOptionalSuccessFlag AS N'Success ðŸš©'
 		, @strOptionalMessageOut AS 'Optional Output Message';
 
 */
@@ -233,10 +220,9 @@ WHERE TABLE_TYPE = N'BASE TABLE'
 	AND t.TABLE_SCHEMA = @ustrSchemaName;
 	 */
 	--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-END CATCH
+	END CATCH
 
-PRINT 
-	'Please check the DB_EXCEPTION_TANK an error has been raised. 
+	PRINT 'Please check the DB_EXCEPTION_TANK an error has been raised. 
 		The query between the lines below will likely get you what you need.
 
 		_____________________________
@@ -264,6 +250,3 @@ PRINT
 
 '
 GO
-
-
-----
