f4ed6d10ced362db876608bf163c2c9a3d062aa9
Dave Babler <dave.babler@msimga.com>
2021-12-02 16:54:01 -0500

    TableExist is ready for AzureSQL

--- AzureSQL/SCHEMAS/DD/PROCEDURES/DD.TableExist.sql
+++ AzureSQL/SCHEMAS/DD/PROCEDURES/DD.TableExist.sql
@@ -10,96 +10,98 @@ GO
 -- =============================================
 -- Author:		    Dave Babler
 -- Create date:     08/25/2020
--- Last Modified:   11/23/2020
+-- Last Modified:   12/02/2021
 -- Description:	    Checks to see if table exists use output boolean for logic flow in other procedures
 -- =============================================
-ALTER
-	
-
- PROCEDURE [DD].[TableExist] @ustrTableName NVARCHAR(64)
-	, @ustrDBName NVARCHAR(64)
-	, --SHOULD BE PASSED IN FROM ANOTHER PROC
-	@ustrSchemaName NVARCHAR(64)
-	, --SHOULD BE PASSED IN FROM ANOTHER PROC
-	@boolSuccessFlag BIT OUTPUT
-	, @ustrMessageOut NVARCHAR(400) = NULL OUTPUT
+CREATE OR ALTER PROCEDURE [DD].[TableExist]
+    @ustrTableName   NVARCHAR(64)
+  , @ustrSchemaName  NVARCHAR(64)
+    --SHOULD BE PASSED IN FROM ANOTHER PROC
+  , @boolSuccessFlag BIT           OUTPUT
+  , @ustrMessageOut  NVARCHAR(400) = NULL OUTPUT
 AS
-SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-BEGIN TRY
-	/** If the table doesn't exist we're going to output a message and throw a false flag,
+    BEGIN TRY
+        /** If the table doesn't exist we're going to output a message and throw a false flag,
      *  ELSE we'll throw a true flag so external operations can commence
      * Dave Babler 2020-08-26  */
-	DECLARE @ustrOutGoingMessageEnd NVARCHAR(48) = N' does not exist, check spelling, try again?';
-	DECLARE @ustrQuotedDB NVARCHAR(128) = N'' + QUOTENAME(@ustrDBName) + '';
-	DECLARE @intRowCount INT;
-	DECLARE @SQLCheckForTable NVARCHAR(1000) = 'SELECT 1 
-                               FROM ' + @ustrQuotedDB + 
-		'.INFORMATION_SCHEMA.TABLES 
+        DECLARE @ustrOutGoingMessageEnd NVARCHAR(48) = N' does not exist, check spelling, try again?';
+        DECLARE @intRowCount INT;
+        DECLARE @SQLCheckForTable NVARCHAR(1000) = 'SELECT 1 
+                               FROM INFORMATION_SCHEMA.TABLES 
                                WHERE TABLE_NAME = @ustrTable 
-                                    AND TABLE_SCHEMA = @ustrSchema'
-		;
-	DROP TABLE IF EXISTS #__beQuiet ;
-	CREATE TABLE #__beQuiet (Shhhh INT)---Suppresses output
+                                    AND TABLE_SCHEMA = @ustrSchema';
 
-	IF DB_ID(@ustrDBName) IS NOT NULL
-	BEGIN 
-	INSERT INTO #__beQuiet
-	EXECUTE sp_executesql @SQLCheckForTable
-		, N'@ustrTable NVARCHAR(64), @ustrSchema NVARCHAR(64)'
-		, @ustrTable = @ustrTableName
-		, @ustrSchema = @ustrSchemaName;
+        PRINT @SQLCheckForTable;
+
+        DROP TABLE IF EXISTS #__beQuiet;
+        CREATE TABLE #__beQuiet
+        (
+            Shhhh INT
+        ) ---Suppresses output
+
+        IF SCHEMA_ID(@ustrSchemaName) IS NOT NULL
+            BEGIN
+                PRINT 'Schema not null;'
+                INSERT INTO #__beQuiet
+                EXECUTE sp_executesql @SQLCheckForTable
+                                    , N'@ustrTable NVARCHAR(64), @ustrSchema NVARCHAR(64)'
+                                    , @ustrTable = @ustrTableName
+                                    , @ustrSchema = @ustrSchemaName;
 
 
 
-	SET @intRowCount = @@ROWCOUNT; 
+                SET @intRowCount = @@ROWCOUNT;
+                PRINT @intRowCount;
 
 
+                IF @intRowCount <> 1
+                    BEGIN
+                        PRINT 'ROWCOUNT NOT 1'
+                        SET @boolSuccessFlag = 0;
+                        SET @ustrMessageOut = @ustrTableName + @ustrOutGoingMessageEnd;
+                    END
+                ELSE
+                    BEGIN
+                        SET @boolSuccessFlag = 1;
+                        SET @ustrMessageOut = NULL;
+                    END
+            END
+        ELSE
+            BEGIN
+                SET @boolSuccessFlag = 0
+                SET @ustrMessageOut = 'The Schema: ' + @ustrSchemaName
+                                      + ' does not exist, check spelling on schema before proceeding?';
+            END
 
-	IF @intRowCount <> 1
-	BEGIN
-		SET @boolSuccessFlag = 0;
-		SET @ustrMessageOut = @ustrTableName + @ustrOutGoingMessageEnd;
-	END
-	ELSE
-	BEGIN
-		SET @boolSuccessFlag = 1;
-		SET @ustrMessageOut = NULL;
-	END
-	END
-	ELSE 
-	BEGIN 
-		SET @ustrMessageOut = @ustrDBName + @ustrOutGoingMessageEnd;
-	END
-	SET NOCOUNT OFF;
-	DROP TABLE #__beQuiet;
-END TRY
+        SET NOCOUNT OFF;
+        DROP TABLE #__beQuiet;
+    END TRY
+    BEGIN CATCH
 
-BEGIN CATCH
-
-	INSERT INTO ERR.DB_EXCEPTION_TANK (
-		[DatabaseName]
-		, [UserName]
-		, [ErrorNumber]
-		, [ErrorState]
-		, [ErrorSeverity]
-		, [ErrorLine]
-		, [ErrorProcedure]
-		, [ErrorMessage]
-		, [ErrorDateTime]
-		)
-	VALUES (
-		DB_NAME()
-		, SUSER_SNAME()
-		, ERROR_NUMBER()
-		, ERROR_STATE()
-		, ERROR_SEVERITY()
-		, ERROR_LINE()
-		, ERROR_PROCEDURE()
-		, ERROR_MESSAGE()
-		, GETDATE()
-		);
-END CATCH;
+        INSERT INTO ERR.DB_EXCEPTION_TANK
+        (
+            [DatabaseName]
+          , [UserName]
+          , [ErrorNumber]
+          , [ErrorState]
+          , [ErrorSeverity]
+          , [ErrorLine]
+          , [ErrorProcedure]
+          , [ErrorMessage]
+          , [ErrorDateTime]
+        )
+        VALUES (DB_NAME()
+              , SUSER_SNAME()
+              , ERROR_NUMBER()
+              , ERROR_STATE()
+              , ERROR_SEVERITY()
+              , ERROR_LINE()
+              , ERROR_PROCEDURE()
+              , ERROR_MESSAGE()
+              , GETDATE());
+    END CATCH;
 /**
  Dynamic SQL in this procedure
 SELECT 1
